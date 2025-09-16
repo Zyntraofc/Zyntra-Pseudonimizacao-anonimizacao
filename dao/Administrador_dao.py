61ecc2346ea448276b326fa67bfc3e5b0f606c55
Eduardo_Farias_cyber <eduradofarias123do@gmail.com>
2025-09-16 17:40:28 -0300

    Adicionando classe dao com métodos de manipulação da tabela administrador e administrador_anonimo

---
+++ dao/Administrador_dao.py
@@ -0,0 +1,66 @@
+#Importação de classes necessárias para manipulação do database
+from dao import Conexao
+from model.Administrador import Administrador
+
+
+#Função para listagem de administradores da tabela administradores
+def listar_administradores():
+    
+    #Atribuindo a variável conn de conexão o valor retornado pelo método 'get_connection' que retorna a conexão com o database
+    conn = Conexao.get_connection()
+    #Verificação se a conexão é nula, se for retorna nulo
+    if not conn:
+        return None
+
+    try:
+        #Abrindo cursor para manipular o databse
+        cur = conn.cursor()
+        #Query a ser executada na tabela Administrador
+        query = "SELECT id_adm, email, hash_senha FROM administrador"
+        #Execução da query pelo cursor
+        cur.execute(query)
+        #Atribuido a uma variável todos os valores e linhas encontrados no database
+        administradores = cur.fetchall()
+        
+        #List comprehension para atribuir objetos tipo Administrador a lista com base em cada linha da tabela Administrador do database
+        admins = [Administrador(a[0], a[1], a[2]) for a in administradores]
+        #Retornando a lista
+        return admins 
+
+    #Em caso de erros, printa que houve erros e o erro, depois retorna lista vazia
+    except Exception as e:
+        print("Erro ao listar:", e)
+        return []
+    #Desconecta antes do retorno
+    finally:
+        cur.close()
+        conn.close()
+
+
+#Função para inserção na tabela administradores_anonimo
+def inserir_administrador(administrador : Administrador):
+    #Atribuindo a variável conn de conexão o valor retornado pelo método 'get_connection' que retorna a conexão com o database
+    conn = Conexao.get_connection()
+    #Verificação de se a conexão não é nula, se for, retorna nulo
+    if not conn:
+        return None
+    
+    try:
+        #Abrindo cursor para manipulação do database
+        cur = conn.cursor()
+        #Insert a ser executado na tabela administrador_anonimo
+        insert = "insert into administrador_anonimo (email, hash_senha) values (%s, %s)"
+        #Execução do insert pelo cursor, e substituição dos valores
+        cur.execute(insert, (administrador.email, administrador.hash_senha))
+        #Retorno de execução
+        if(conn != None):
+            return False
+        else:
+            return True
+    #Em caso de erro retorna qual é o erro
+    except Exception as e:
+        print("Erro ao inserir" + e)
+    #Desconecta antes do retorno
+    finally:
+        conn.close
+        cur.close
